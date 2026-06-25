673dda38a12f9760e215a1a6b77db1cf540a0ece
Uttam Singh <uttamsinghus100@gmail.com>
2026-06-25 16:53:27 +0530

    fix(category):  Updated async function without the next parameter in the pre-hook

--- src/models/Category.js
+++ src/models/Category.js
@@ -39,14 +39,13 @@ const categorySchema = new mongoose.Schema({
      timestamps:true
 });
 
-categorySchema.pre('save',function next(){
-     if(!this.slug){
-          this.slug=this.name
+categorySchema.pre('save', async function () {
+     if (!this.slug) {
+          this.slug = this.name
                .toLowerCase()
                .replace(/[^a-z0-9]+/g, '-')
-               .replace(/^-+|-+$/g, '')
+               .replace(/^-+|-+$/g, '');
      }
-     next()
-})
+});
 
 module.exports = mongoose.model("Category", categorySchema);
