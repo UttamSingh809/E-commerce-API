1fa95988bdba03a5d823a4560ec2aa6df24ffa29
Uttam Singh <uttamsinghus100@gmail.com>
2026-06-25 22:21:58 +0530

    feat(product): define product routes and map to controller endpoints

---
+++ src/routes/product.routes.js
@@ -0,0 +1,12 @@
+const express=require('express')
+const router=express.Router()
+const productController=require('../controllers/product.controller')
+const {protect, adminOnly}=require('../middleware/auth.middleware')
+
+router.get('/', productController.getAllProducts)
+router.get('/:id', productController.getProduct)
+
+router.post('/',protect,adminOnly,productController.createProduct)
+router.put('/:id',protect,adminOnly,productController.updateProduct)
+router.delete('/:id',protect,adminOnly,productController.deleteProduct)
+module.exports=router
