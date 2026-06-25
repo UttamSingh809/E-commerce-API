7e537d0b8973cbec1c655f3610b2b06a41174665
Uttam Singh <uttamsinghus100@gmail.com>
2026-06-25 12:37:36 +0530

    feat(category): Added Category Routes

---
+++ src/routes/category.routes.js
@@ -0,0 +1,14 @@
+const express = require('express')
+const router = express.Router()
+const categoryController=require('../controllers/category.controller')
+const {protect,adminOnly}=require('../middleware/auth.middleware')
+
+router.get('/',categoryController.getAllCategories)
+router.get('/:id',categoryController.getCategory)
+router.get('/:id/products',categoryController.getCategoryProducts)
+
+router.get('/',protect,adminOnly,categoryController.createCategory)
+router.put('/:id',protect,adminOnly,categoryController.updateCategory)
+router.delete('/:id',protect,adminOnly,categoryController.deleteCategory)
+
+module.exports=router
