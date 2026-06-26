6dd7f8f2187ef6da6edf9e9c8cd15beae5360a81
Uttam Singh <uttamsinghus100@gmail.com>
2026-06-26 17:15:39 +0530

    feat(cart): define product routes and map to controller endpoints

---
+++ src/routes/cart.routes.js
@@ -0,0 +1,14 @@
+const express = require('express')
+const router = express.Router()
+const cartController = require('../controllers/cart.controller')
+const { protect } = require('../middleware/auth.middleware')
+
+router.use(protect)
+
+router.get('/', cartController.getCart)
+router.post('/items', cartController.addToCart)
+router.put('/items/:productId',cartController.updateCartItem)
+router.delete('/items/:productId',cartController.removeFromCart)
+router.delete('/',cartController.clearCart)
+
+module.exports = router
