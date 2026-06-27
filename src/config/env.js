a0d35c7deca6c9026e8b130424bf596ffee4a89b
Uttam Singh <uttamsinghus100@gmail.com>
2026-06-27 13:03:05 +0530

    feat(payment): Enables fetching paypal env data from .env

--- src/config/env.js
+++ src/config/env.js
@@ -5,5 +5,13 @@ module.exports = {
     port: process.env.PORT || 3000,
     mongoUri: process.env.MONGODB_URI,
     jwtSecret: process.env.JWT_SECRET,
-    nodeEnv: process.env.NODE_ENV
+    nodeEnv: process.env.NODE_ENV,
+    jwtExpire: process.env.JWT_EXPIRE,
+
+    paypal:{
+        clientId: process.env.PAYPAL_CLIENT_ID,
+        clientSecret: process.env.PAYPAL_CLIENT_SECRET,
+        mode: process.env.PAYPAL_MODE || 'sandbox',
+        currency: process.env.PAYPAL_CURRENCY || 'USD'
+    }
 };
